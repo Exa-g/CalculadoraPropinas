3fa23cb5363f942a4d68321cfc935f4d25e44ca5
Exael G <exaig03@gmail.com>
2024-01-29 21:42:17 -0600

    Descripcion detallada de los cambios realizados

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,21 @@
+def obtener_numero(mensaje):
+    while True:
+        try:
+            return float(input(mensaje))
+        except ValueError:
+            print("Error: Ingresa un numero valido.")
+            
+def obtener_entero_positivo(mensaje):
+    while True:
+        try:
+            valor = int(input(mensaje))
+            if valor >= 0:
+                return valor
+            else:
+                print("Error: Ingresa un numero entero positivo.")
+        except ValueError:
+            print("Error: Ingresar un numero entero valido.")
+         
 def calculadora_propina(monto, porcentaje):
     propina = monto * (porcentaje / 100)
     return propina
@@ -12,10 +30,10 @@ def main():
     print("Bien benido a la Calculadora de Propinas")
     
     #Ingresa el monto de la cuenta
-    monto_cuenta = float(input("Ingresa el monto de la cuenta: $"))
+    monto_cuenta = obtener_numero("Ingresa el monto de la cuenta: $")
     
     #Ingresar el porcentaje de propina deseada
-    porcentanje_propina = float(input("Ingresa el porcentaje de propina (porcentaje deseado): " + "%"))
+    porcentanje_propina = obtener_numero("Ingresa el porcentaje de propina (porcentaje deseado): ")
     
     #Calcula la propina
     propina = calculadora_propina(monto_cuenta, porcentanje_propina)
@@ -25,6 +43,7 @@ def main():
     
     #Calcula el total con o sin redondeo
     total = monto_cuenta + propina
+    
     if redondear:
         total = redondear_total(total)
         
@@ -33,11 +52,14 @@ def main():
     
     #Calcular y mostrar el total final
     if dividir:
-        personas = int(input("Ingresa el numero de personas: "))
+        personas = obtener_entero_positivo("Ingresa el numero de personas: ")
         total_por_persona = dividir_cuenta(total, personas)
-        print(f"\nCada persona debe pagar: ${total_por_persona: 2f}")
+        print("\n=== Resultado ===")
+        print(f"Total a pagar (incluyendo propina): ${total: .2f}")
+        print(f"Total por persona (dividido entre {personas} personas): ${total_por_persona: .2f}")
     else:
-        print(f"\nEl total a pagar es: ${total:.2f}")
+        print("\n=== Resultado ===")
+        print(f"Total a pagar (incluyendo propina): ${total:.2f}")
         
-        if __name__ == "__main__":
-            main()
+if __name__ == "__main__":
+    main()
